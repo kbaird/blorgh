17cd2c866b189bcf6742a78c66dc08d29b064d08
Kevin C. Baird <kcbaird@world.oberlin.edu>
2014-06-24 11:01:26 -0700

    Use rspec for integration specs

--- MyEngine/lib/my_engine/engine.rb
+++ MyEngine/lib/my_engine/engine.rb
@@ -5,6 +5,7 @@ module MyEngine
 
     config.generators do |g|
       g.test_framework      :rspec,        fixture: false
+      g.integration_tool    :rspec
       g.fixture_replacement :factory_girl, dir:     'spec/factories'
       g.assets false
       g.helper false
